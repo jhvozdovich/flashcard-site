4e31231b8ddf28223cbe65d57fc1269038334ae5
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-03-24 13:46:33 -0700

    add flashcards that have buttons that toggle between the term and definition

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $("#javascript").click(function(){
+    $("#javascript .term").toggle();
+    $("#javascript .definition").toggle();
+  })
+  $("#operators").click(function(){
+    $("#operators .term").toggle();
+    $("#operators .definition").toggle();
+  })
+})
